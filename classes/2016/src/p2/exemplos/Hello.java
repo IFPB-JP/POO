6321a3da0213d1b541e49041e004e4326412887d
fcopetroniomedeiros <fcopetroniomedeiros@gmail.com>
2016-09-22 10:12:58 -0300

    Exemplos da aula de 22 de setembro de 2016

---
+++ classes/2016/src/p2/exemplos/Hello.java
@@ -0,0 +1,15 @@
+package p2.exemplos;
+ 
+/*
+ * O primeiro programa em Java: Hello World
+ */
+ 
+// Todo programa tem um ponto de entrada:
+// o "método" main de alguma "classe"
+ 
+public class Hello {
+ 
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
